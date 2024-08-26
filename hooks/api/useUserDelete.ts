9c6c15c3866e3d3a8effa89de9aa1901ed237873
ihorbalan <i.balan@seedium.io>
2024-08-26 14:42:54 +0300

    feat: add user list screen

--- hooks/api/useUserDelete.ts
+++ hooks/api/useUserDelete.ts
@@ -1,10 +1,27 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { UserListResponse } from "@/hooks/api/useUserList";
+import { User } from "@/types/user.type";
 
 export const useUserDelete = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (id: number) =>
       fetch(`https://reqres.in/api/users/${id}`, {
         method: "DELETE",
       }).then((res) => res.status),
+    onMutate: (id) => {
+      queryClient.setQueriesData(
+        { queryKey: ["users"] },
+        (oldData: UserListResponse | undefined) => {
+          if (!oldData) return;
+
+          return {
+            ...oldData,
+            data: oldData.data.filter((user: User) => user.id !== id),
+          };
+        },
+      );
+    },
   });
 };
--- hooks/api/useUserList.ts
+++ hooks/api/useUserList.ts
@@ -1,9 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { stringifyQueryParams } from "@/helpers/stringifyQueryParams";
+import { User } from "@/types/user.type";
 
 type UserListParams = {
   page?: number;
   per_page?: number;
+};
+
+export type UserListResponse = {
+  data: User[];
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
 };
 
 export const useUserList = (params?: UserListParams) => {
@@ -12,6 +21,7 @@ export const useUserList = (params?: UserListParams) => {
     queryFn: () =>
       fetch(
         `https://reqres.in/api/users?${stringifyQueryParams(params ?? {})}`,
-      ).then((res) => res.json()),
+      ).then((res) => res.json() as Promise<UserListResponse>),
+    staleTime: 5000,
   });
 };
