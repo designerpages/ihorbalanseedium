2a9e8e435270c2cb6a14452cbdcd1041b21541a6
ihorbalanseedium <74588655+ihorbalanseedium@users.noreply.github.com>
2024-08-26 14:44:03 +0300

    feat: add user list screen (#4)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,10 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { PaperProvider } from "react-native-paper";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -18,7 +13,6 @@ const queryClient = new QueryClient();
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -35,12 +29,12 @@ export default function RootLayout() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <PaperProvider>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
-      </ThemeProvider>
+      </PaperProvider>
     </QueryClientProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,182 @@
+import { StyleSheet, Text, ScrollView, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useUserList } from "@/hooks/api/useUserList";
+import {
+  ActivityIndicator,
+  Avatar,
+  Button,
+  Card,
+  IconButton,
+  Modal,
+  Portal,
+  Menu,
+  Divider,
+} from "react-native-paper";
+import { useMemo, useState } from "react";
+import PaginationDot from "react-native-animated-pagination-dot";
+import { useTheme } from "react-native-paper";
+import { useUserDelete } from "@/hooks/api/useUserDelete";
+import { User } from "@/types/user.type";
+
+export default function HomeScreen() {
+  const theme = useTheme();
+  // state
+  const [activeUser, setActiveUser] = useState<null | User>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [page, setPage] = useState(0);
+
+  // hooks
+  const { data, isLoading } = useUserList({
+    page: page + 1,
+    per_page: 8,
+  });
+  const { mutate: deleteUser } = useUserDelete();
+
+  const [hasPrevPage, hasNextPage] = useMemo(
+    () => [page - 1 >= 0, page + 1 < (data?.total_pages ?? 0)],
+    [data?.total_pages, page],
+  );
+
+  const handleDeleteUser = () => {
+    if (!activeUser) return;
+
+    setActiveUser(null);
+    setIsMenuVisible(false);
+    deleteUser(activeUser.id);
+  };
+
+  return (
+    <View style={styles.rootContainer}>
+      <Portal>
+        <Modal
+          visible={isModalVisible}
+          onDismiss={() => setIsModalVisible(false)}
+          style={{ margin: 20 }}
+          contentContainerStyle={{ padding: 20, backgroundColor: "white" }}
+        >
+          <Text>
+            Since the application using public API, some CRUD operations won't
+            result in any real changes. Though I have implemented optimistic
+            update and delete which should give you a feel of real-time updates.
+          </Text>
+        </Modal>
+      </Portal>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.content}>
+          <ScrollView scrollEventThrottle={16}>
+            <Button onPress={() => setIsModalVisible(true)}>
+              Additional Info
+            </Button>
+            <View style={{ flex: 1 }}>
+              {isLoading && (
+                <ActivityIndicator
+                  style={{ marginTop: 20 }}
+                  animating={true}
+                  size={"large"}
+                />
+              )}
+              {data?.data.map((user) => (
+                <Card.Title
+                  key={user.id}
+                  title={user.first_name}
+                  subtitle={user.email}
+                  left={(props) => (
+                    <Avatar.Image size={32} source={{ uri: user.avatar }} />
+                  )}
+                  right={(props) => (
+                    <IconButton
+                      {...props}
+                      icon="dots-vertical"
+                      onPress={(e) => {
+                        setActiveUser(user);
+                        e.target.measure((_, __, width, ___, px, py) => {
+                          setPosition({ x: px + width, y: py });
+                        });
+                        setIsMenuVisible(true);
+                      }}
+                    />
+                  )}
+                />
+              ))}
+            </View>
+          </ScrollView>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              disabled={!hasPrevPage}
+              onPress={() => setPage((prev) => prev - 1)}
+            >
+              Prev
+            </Button>
+            <PaginationDot
+              activeDotColor={theme.colors.primary}
+              curPage={page}
+              maxPage={data?.total_pages ?? 0}
+            />
+            <Button
+              disabled={!hasNextPage}
+              onPress={() => setPage((prev) => prev + 1)}
+            >
+              Next
+            </Button>
+          </View>
+        </View>
+      </SafeAreaView>
+      <Menu
+        visible={isMenuVisible}
+        anchorPosition={"bottom"}
+        onDismiss={() => setIsMenuVisible(false)}
+        anchor={position}
+      >
+        <Menu.Item
+          leadingIcon={"eye-outline"}
+          onPress={() => {}}
+          title="View"
+        />
+        <Divider />
+        <Menu.Item leadingIcon={"pencil"} onPress={() => {}} title="Edit" />
+        <Divider />
+        <Menu.Item
+          leadingIcon={"delete"}
+          onPress={handleDeleteUser}
+          title="Delete"
+        />
+      </Menu>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    gap: 16,
+    overflow: "hidden",
+  },
+});
